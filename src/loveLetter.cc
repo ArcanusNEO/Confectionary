e47960a65589ac5931e193951421f2f2867173d2
ArcanusNEO <lucasding@foxmail.com>
2022-02-05 01:12:35 +0800

    .

--- src/loveLetter.cc
+++ src/loveLetter.cc
@@ -41,7 +41,7 @@ loveLetter::loveLetter(QWidget* parent)
   pLoveGif.reset(new QMovie(":/pic/love"));
   pLb->setMovie(pLoveGif.get());
   pLoveGif->start();
-  pLb->setGeometry(0, 0, 0, 0);
+  pLb->setGeometry((windowWidth - loveWidth) / 2, 0, 0, 0);
   for (int i = 0; i < 2; ++i) {
     pBtn[i].reset(new QToolButton(this));
     pBtn[i]->setFixedSize(btnWidth, btnHeight);
