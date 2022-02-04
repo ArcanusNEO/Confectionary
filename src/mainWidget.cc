ae52b10e00fca98c4325333930d45adf1bb58b40
ArcanusNEO <lucasding@foxmail.com>
2022-02-04 15:16:57 +0800

    .

--- src/mainWidget.cc
+++ src/mainWidget.cc
@@ -1,21 +1,22 @@
 #include "mainWidget.hh"
 #include <QIcon>
+#include <QPixmap>
 
 mainWidget::mainWidget(QWidget* parent)
   : QWidget(parent) {
-  enum { windowWidth = 400, windowHeight = 200, btnWidth = 100, btnHeight = 35 };
+  enum { windowWidth = 400, windowHeight = 200, btnWidth = 200, btnHeight = 35 };
   setWindowFlags(windowFlags() | Qt::FramelessWindowHint);
   setFixedSize(windowWidth, windowHeight);
+  QPixmap pix[2];
   for (int i = 0; i < 2; ++i) {
     pBtn[i].reset(new QPushButton(this));
     pBtn[i]->setFixedSize(btnWidth, btnHeight);
+    pix[i].load(QString("pic/btn") + QString(i + '0') + QString(".png"));
+    pix[i].scaled(btnWidth, btnHeight, Qt::AspectRatioMode::KeepAspectRatio);
+    pBtn[i]->setIcon(pix[i]);
   }
   pBtn[0]->setGeometry((windowWidth / 2 - btnWidth) / 2, windowHeight - btnHeight, 0, 0);
-  pBtn[0]->setIcon(QIcon("/home/lucas/src/Confectionary/pic/b站害羞.png"));
-  pBtn[0]->setText(tr("我愿意"));
   pBtn[1]->setGeometry(3 * windowWidth / 4 - btnWidth / 2, windowHeight - btnHeight, 0, 0);
-  pBtn[1]->setIcon(QIcon("/home/lucas/src/Confectionary/pic/b站傲娇.png"));
-  pBtn[1]->setText(tr("我不！"));
 }
 
 mainWidget::~mainWidget() {}
